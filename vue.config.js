86ff8e8b9f1bf43d8e0c17b7fd8b63c94744c608
changgengwu <a8040333@gmail.com>
2021-09-27 15:44:28 +0800

    git page setting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   devServer: {
     proxy: "http://localhost:8080/orderApp/api",
+    publicPath:
+      process.env.NODE_ENV === "production" ? "/OrderApp-vue-cli/" : "/",
   },
 };
